16641fe0611ee29f47e883464403b7c6104cfe52
FashJosh <fashina2000@gmail.com>
2025-05-09 14:10:45 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const menuButton = document.querySelector('.menu-button');
+const menuLinks = document.querySelector('.menu-links');
+
+menuButton.addEventListener('click', () => {
+  menuLinks.classList.toggle('show');
+});
